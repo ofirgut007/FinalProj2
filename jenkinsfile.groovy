e7697094d23c4876424d33ec66f7c584e609529e
ofirgut007 <ofirgut007@gmail.com>
2018-02-05 18:52:01 +0200

    added slack

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,6 +1,7 @@
 node ("master") {
 	def mvnHome
 	stage('Checkout') { // for display purposes
+		notifySlack()
 		// Get some code from a GitHub repository
 		git 'https://github.com/ofirgut007/spring-boot-examples.git'
 		// Get the Maven tool.
@@ -45,7 +46,27 @@ node ("master") {
 		}
     }
     stage ('Final') {
-
+		notifySlack(currentBuild.result)
 
     }
+}
+def notifySlack(String buildStatus = 'STARTED') {
+	    // Build status of null means success.
+	    buildStatus = buildStatus ?: 'SUCCESS'
+
+	    def color
+
+	    if (buildStatus == 'STARTED') {
+			color = '#D4DADF'
+	    } else if (buildStatus == 'SUCCESS') {
+			color = '#BDFFC3'
+	    } else if (buildStatus == 'UNSTABLE') {
+			color = '#FFFE89'
+	    } else {
+			color = '#FF9FA1'
+	    }
+
+	    def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
+
+	    slackSend(color: color, message: msg)
 }
